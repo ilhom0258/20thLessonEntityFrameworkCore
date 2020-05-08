294e202b7c905817238d2b99e4b82e24064448b6
Ilhom <ilhom0258@gmail.com>
2020-05-09 04:13:17 +0500

    Done

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 using System;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Transactions;
 
 namespace ConsoleApp2
 {
@@ -13,6 +16,7 @@ namespace ConsoleApp2
 
             while ( working)
             {
+                Console.Clear(); 
                 Console.Write("1.Create\n2.Read\n3.Update\n4.Delete\n5.Exit\nYour Choice : ");
                 int choice; 
                 if ( int.TryParse(Console.ReadLine(), out choice))
@@ -25,9 +29,9 @@ namespace ConsoleApp2
                                 break;
                             case 2:Read(); 
                                 break;
-                            case 3:
+                            case 3:Update();
                                 break;
-                            case 4:
+                            case 4:Delete(); 
                                 break;
                             case 5:working = false;
                                 break;
@@ -42,9 +46,12 @@ namespace ConsoleApp2
             {
                 using ( AcademyContext db = new AcademyContext())
                 {
-                    var itemName = Console.ReadLine();
-                    var itemPrice = Console.ReadLine();
-                    var category = Console.ReadLine();
+                    Console.Write("Item Name: "); 
+                    var itemName = Console.ReadLine().Trim();
+                    Console.Write("Item Price: ");
+                    var itemPrice = Console.ReadLine().Trim();
+                    Console.Write("Item Category: "); 
+                    var category = Console.ReadLine().Trim();
                     Item item = new Item { Name = itemName, Category = category, Price = itemPrice};
                     db.Item.Add(item);
                     db.SaveChanges(); 
@@ -55,28 +62,96 @@ namespace ConsoleApp2
                 Console.WriteLine(ex.Message); 
             }
         }
-        private static void Read()
+        private static void Read(string com = "null")
         {
             try
             {
                 using (AcademyContext db = new AcademyContext())
                 {
-                    var shops = db.Shop.ToList();
                     var items = db.Item.ToList();
-                    foreach( var shop in shops)
+                    // Show items; 
+                    Console.WriteLine("Item ID..................Item Name...............Item Price..................Item Category"); 
+                    foreach ( var item in items)
                     {
-                        Console.WriteLine($"{shop.Id}.....${shop.Item}.....${shop.ItemId}"); 
+                        Console.WriteLine($"{item.Id}........{item.Name}.........{item.Price}........{item.Category}"); 
                     }
-                    foreach( var item in items)
+                    if ( com == "null")
                     {
-                        Console.WriteLine($"{item.Id}.....{item.Name}....{item.Price}....{item.Category}"); 
+                        PressAnyKey();
                     }
+
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
+        }
+        private static void Update()
+        {
+            try
+            {
+                using (AcademyContext db = new AcademyContext())
+                {
+                    Read("update"); 
+                    Console.Write("Item Id: "); 
+                    int id = int.Parse(Console.ReadLine());
+
+                    var item = db.Item.Find(id); 
+
+                    if ( item != null)
+                    {
+                        Console.Write("Item Price: ");
+                        var price = Console.ReadLine().Trim();
+                        item.Price = price;
+                        db.SaveChanges();
+                        PressAnyKey();
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        private static void Delete()
+        {
+            try
+            {
+                using (AcademyContext db = new AcademyContext())
+                {
+                    Read("delete");
+                    int id = -1;
+                    bool t = true; 
+                    while ( t )
+                    {
+                        Console.Write("ID: "); 
+                        if ( int.TryParse(Console.ReadLine(), out id))
+                        {
+                            var item = db.Item.Find(id);
+                            if (item != null)
+                            {
+                                db.Item.Remove(item);
+                                db.SaveChanges(); 
+                                Console.WriteLine("Item Removed");
+                                PressAnyKey();
+                            }
+                            break; 
+                        }
+                    }
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        private static void PressAnyKey()
+        {
+            Console.WriteLine("Press any key to continue.....");
+            Console.ReadKey(); 
         }
     }
    
