7df200e3d6c60d3bb18d6e401298615522205b8a
Ilhom <ilhom0258@gmail.com>
2020-05-09 02:36:28 +0500

    Project init && Item class add

---
+++ ConsoleApp2/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+    }
+}
